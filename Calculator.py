093401942f0e0ce1045f75c525c7d4e51097aef4
rsegura24 <riseba36@gmail.com>
2020-02-24 11:51:22 +0100

    first comment

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 11:30:57 2020
+
+@author: rikis
+"""
+
+class Calculator:
+    
+    urbag_constant = 67.8
+    
+    def sum(a,b):
+        return a + b
---
+++ test.py
@@ -0,0 +1,3 @@
+from Calculator import Calculator
+
+result = Calculator.sum(3, 4)
